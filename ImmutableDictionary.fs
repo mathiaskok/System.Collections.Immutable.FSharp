2bc4922b595f1d4036281251272ff0e7bcd7e9fd
Mathias Kok <mak@cBrain.net>
2022-08-31 17:12:17 +0200

    Initial commit. Contains: - ImmutableList - ImmutableDictionary

---
+++ ImmutableDictionary.fs
@@ -0,0 +1,44 @@
+﻿module System.Collections.Immutable.FSharp.ImmutableDictionary
+
+open System.Collections.Generic
+open System.Collections.Immutable
+
+let ofStructTupleSeq s =
+  ImmutableDictionary.ToImmutableDictionary
+    (
+      s,
+      (fun struct(k,_) -> k),
+      (fun struct(_,v) -> v)
+    )
+
+let ofTupleSeq s =
+  ImmutableDictionary.ToImmutableDictionary (s, fst, snd)
+
+let ofKeyValuePairSeq (s: KeyValuePair<'k,'v> seq) =
+  ImmutableDictionary.ToImmutableDictionary s
+
+let add key value (dict:ImmutableDictionary<'k,'v>) = dict.Add(key,value)
+
+let setItem key value (dict:ImmutableDictionary<'k,'v>) = dict.SetItem(key,value)
+
+let count (dict:ImmutableDictionary<'k,'v>) = dict.Count
+
+let keys (dict:ImmutableDictionary<'k,'v>) = dict.Keys
+
+let values (dict:ImmutableDictionary<'k,'v>) = dict.Values
+
+
+module Patterns =
+  
+  [<return: Struct>]
+  let (|Empty|_|) (d:ImmutableDictionary<'k,'v>) =
+    match d.Count with
+    | 0 -> ValueSome Empty
+    | _ -> ValueNone
+
+  [<return: Struct>]
+  let (|TakeElem|_|) k (d:ImmutableDictionary<'k,'v>) =
+    match d.TryGetValue k with
+    | (true,v) -> 
+      ValueSome (struct(v,d.Remove k))
+    | _ -> ValueNone
---
+++ ImmutableList.fs
@@ -0,0 +1,48 @@
+﻿module System.Collections.Immutable.FSharp.ImmutableList
+
+open System
+open System.Collections.Immutable
+
+module Patterns =
+  
+  [<return: Struct>]
+  let (|Empty|_|) (l:ImmutableList<'a>) =
+    match l.Count with
+    | 0 -> ValueSome Empty
+    | _ -> ValueNone
+
+  [<return: Struct>]
+  let (|TakeElem|_|) i (l:ImmutableList<'a>) =
+    match i-1 > l.Count with
+    | true -> ValueNone
+    | false -> 
+      let li = l.[i]
+      let l = l.RemoveAt i
+      ValueSome (struct(li,l))
+
+  [<return: Struct>]
+  let (|TakeFirst|_|) l =
+    match l with
+    | TakeElem 0 lp -> ValueSome lp
+    | _ -> ValueNone
+
+  [<return: Struct>]
+  let (|TakeLast|_|) (l:ImmutableList<'a>) =
+    let i = l.Count - 1
+    match l with
+    | TakeElem i (v,rem) -> ValueSome (struct(rem,v))
+    | _ -> ValueNone
+
+  [<return: Struct>]
+  let rec (|TakeFirstN|_|) n (l:ImmutableList<'a>) =
+    match n with
+    | _ when n < 0 -> raise (ArgumentOutOfRangeException "n must not be negative")
+    | 0 -> ValueSome (struct([],l))
+    | _ ->
+      let n = n-1
+      match l with
+      | TakeFirst (e,l) -> 
+        match (|TakeFirstN|_|) (n-1) l with
+        | ValueNone -> ValueNone
+        | ValueSome (es,l) -> ValueSome (struct(e::es,l))
+      | _ -> ValueNone
